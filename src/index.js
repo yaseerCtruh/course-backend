68081099980a0160e5a4ea258de9316c311fa205
yaseerCtruh <yaseer@ctruh.com>
2024-12-16 16:01:15 +0530

    added cors and cookie-parser dependencies and updated package-lock.json and package.json files accordingly. Updated app.js to use cors and cookie-parser middleware. Updated index.js to listen on a specific port after connecting to the database.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-
+import { app } from "./app.js";
 dotenv.config({
   path: "./env",
 });
 
-connectDB();
+const PORT = process.env.PORT || 8000;
+connectDB()
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server is running on port ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(`MONGODB Connection ERROR src/index: ${error}`);
+  });
