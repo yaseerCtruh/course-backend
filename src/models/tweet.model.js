fd7762a4b4ebab92e7096eb23ad38656364caaab
yaseerCtruh <yaseer@ctruh.com>
2025-01-27 12:18:49 +0530

    `Added content and owner fields to tweet schema`

--- src/models/tweet.model.js
+++ src/models/tweet.model.js
@@ -1,7 +1,13 @@
 import { Schema, model } from "mongoose";
 
 const tweetSchema = new Schema(
-  {},
+  {
+    content: { type: String, required: true },
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
   {
     timestamps: true,
   },
