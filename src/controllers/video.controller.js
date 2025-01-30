0405ce0b814164cdc954516e30a526d0b03fec97
yaseerCtruh <yaseer@ctruh.com>
2025-01-30 17:01:57 +0530

    Add video retrieval functionality and enhance video schema with required fields

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -61,6 +61,19 @@ const publishAVideo = asyncHandler(async (req, res) => {
 const getVideoById = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
   //TODO: get video by id
+
+  if (!videoId) {
+    throw new ApiError(400, "Video ID is required!");
+  }
+
+  const video = await Video.findById(videoId);
+  if (!video) {
+    throw new ApiError(404, "Video not found!");
+  }
+
+  return res
+    .status(200)
+    .json(new ApiResponse(200, video, "Video fetched successfully!"));
 });
 
 const updateVideo = asyncHandler(async (req, res) => {
--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,19 +1,24 @@
 import { Schema, model } from "mongoose";
 import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
-const videoSchema = new Schema({
-  videoFile: {
-    type: String, // cloudinary url
-    required: true,
+const videoSchema = new Schema(
+  {
+    videoFile: {
+      type: String, // cloudinary url
+      required: true,
+    },
+    thumbnail: { type: String, required: true },
+    title: { type: String, required: true },
+    description: { type: String, required: true },
+    duration: { type: Number, required: true },
+    views: { type: Number, default: 0 },
+    isPublic: { type: Boolean, default: true },
+    owner: { type: Schema.Types.ObjectId, ref: "User" },
   },
-  thumbnail: { type: String, required: true },
-  title: { type: String, required: true },
-  description: { type: String, required: true },
-  duration: { type: Number, required: true },
-  views: { type: Number, default: 0 },
-  isPublic: { type: Boolean, default: true },
-  owner: { type: Schema.Types.ObjectId, ref: "User" },
-});
+  {
+    timestamps: true,
+  },
+);
 
 videoSchema.plugin(mongooseAggregatePaginate);
 export const Video = model("Video", videoSchema);
