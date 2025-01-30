05c4759dffcf686e5389e737f99982d162152c18
yaseerCtruh <yaseer@ctruh.com>
2025-01-30 19:58:39 +0530

    Add toggle publish status functionality with authorization checks and error handling

--- src/controllers/video.controller.js
+++ src/controllers/video.controller.js
@@ -169,6 +169,40 @@ const deleteVideo = asyncHandler(async (req, res) => {
 
 const togglePublishStatus = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
+
+  if (!videoId) {
+    throw new ApiError(400, "Video ID is required!");
+  }
+
+  const video = await Video.findById(videoId);
+  if (!video) {
+    throw new ApiError(404, "Video not found!");
+  }
+
+  const videoOwner = video.owner.toString();
+  const userId = req?.user?._id.toString();
+  if (videoOwner !== userId) {
+    throw new ApiError(403, "You are not authorized to update this video!");
+  }
+
+  const updatedStatus = await Video.findByIdAndUpdate(
+    videoId,
+    { $set: { isPublic: !video?.isPublic } },
+    { new: true },
+  );
+  if (!updatedStatus) {
+    throw new ApiError(500, "Failed to update video status!");
+  }
+
+  return res
+    .status(200)
+    .json(
+      new ApiResponse(
+        200,
+        updatedStatus,
+        "Video publish status updated successfully!",
+      ),
+    );
 });
 
 export {
